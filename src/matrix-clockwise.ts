20b3a14fd6d06ab8bdf7a9213765b64fd08e78b5
Jared Blackman <jared.blackman@blinq.me>
2022-07-12 09:22:14 +1000

    condensed solution

--- src/matrix-clockwise.ts
+++ src/matrix-clockwise.ts
@@ -5,33 +5,14 @@ export type Matrix = number[][];
  */
 export function matrixToClockwise(value: Matrix): Matrix {
   // get dimensions of tensor
-  const dimA: number = value.length;
-  const dimB: number = value[0].length;
+  const dimALength: number = value.length;
+  const dimBLength: number = value[0].length;
 
-  // push all information to a buffer
-  const buffer: number[] = [];
-  for (let i = 0; i < value.length; i++) {
-    const arr: number[] = value[i];
-    for (let j = 0; j < arr.length; j++) {
-      buffer.push(arr[j]);
-    }
-  }
-
-  // re-order elements in buffer
-  const reordered: number[] = [];
-  for (let i = 0; i < dimB; i++) {
-    for (let j = dimA - 1; j >= 0; j--) {
-      const element: number = buffer[j * dimB + i];
-      reordered.push(element);
-    }
-  }
-
-  // re-build matrix with re-shapped dimensions
   const output: Matrix = [];
-  for (let i = 0; i < dimB; i++) {
+  for (let j = 0; j < dimBLength; j++) {
     const arr: number[] = [];
-    for (let j = 0; j < dimA; j++) {
-      const element: number = reordered[i * dimA + j];
+    for (let i: number = dimALength - 1; i >= 0; i--) {
+      const element: number = value[i][j];
       arr.push(element);
     }
     output.push(arr);
