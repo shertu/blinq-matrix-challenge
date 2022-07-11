f4435f25fe432ef5d2578643615b01c5cd3a307a
Jared Blackman <jared.blackman@blinq.me>
2022-07-11 21:06:06 +1000

    added an additional 3 x 5 test case

--- src/matrix-clockwise.spec.ts
+++ src/matrix-clockwise.spec.ts
@@ -49,4 +49,21 @@ describe('matrixToClockwise', () => {
     const actual: Matrix = matrixToClockwise(matrix);
     expect(actual).toEqual(expected);
   });
+
+  it('3 x 5 matrix', () => {
+    const matrix: Matrix = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15],
+    ];
+    const expected: Matrix = [
+      [11, 6, 1],
+      [12, 7, 2],
+      [13, 8, 3],
+      [14, 9, 4],
+      [15, 10, 5],
+    ];
+    const actual: Matrix = matrixToClockwise(matrix);
+    expect(actual).toEqual(expected);
+  });
 });
